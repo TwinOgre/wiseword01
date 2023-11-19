4900206db6da8910ab6bb6452a9ab29a63343b60
twinogre <glad2you@naver.com>
2023-11-19 16:14:26 +0900

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//== 명언 앱 ==
+//명령) 종료
+        String sd="";
+        System.out.println("== 명언 앱 ==");
+        Scanner scan = new Scanner(System.in);
+        sd = new String();
+
+    }
+}
